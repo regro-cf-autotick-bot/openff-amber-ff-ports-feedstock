99655ae7b6b9432efb8af45a387532b5b4afad63
Matthew W. Thompson <mattwthompson@protonmail.com>
2023-11-06 13:47:22 -0600

    Skip Amber-style impropers

--- recipe/test_loadable.py
+++ recipe/test_loadable.py
@@ -1,5 +1,8 @@
 from openff.toolkit import ForceField
 
-for improper in [True, False]:
+for off_improper in [True]:
     for version in range(1, 5):
-        ForceField(f"ff14sb_{'off_impropers_' if improper else ''}0.0.{version}.offxml")
+        ForceField(
+            f"ff14sb_{'off_impropers_' if off_improper else ''}"
+            f"0.0.{version}.offxml"
+        )
